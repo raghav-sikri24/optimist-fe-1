ea3328d362a2de29529a1d658ee427ebd5e252bf
Raghav Sikri <raghav2420@gmail.com>
2026-02-09 23:09:12 +0530

    wip

--- src/components/products/ComparisonSection.tsx
+++ src/components/products/ComparisonSection.tsx
@@ -239,15 +239,15 @@ export const ComparisonSection = memo(function ComparisonSection() {
       </div>
 
       {/* White Background Section for bottom half of AC images */}
-      <div className="relative bg-white h-[65px] md:h-[175px]" />
+      <div className="relative bg-white h-[95px] md:h-[225px]" />
 
       {/* AC Product Images - Single merged image positioned to span both sections */}
       <div
         ref={imagesRef}
-        className="absolute bottom-0 left-0 right-7 z-[20] will-change-[transform,opacity]"
+        className="absolute bottom-0 left-0 right-10 z-[20] will-change-[transform,opacity]"
       >
         <div className="w-full max-w-[1440px] mx-auto px-4 md:px-6 lg:px-12">
-          <div className="relative w-full h-[180px] md:h-[350px]">
+          <div className="relative w-full h-[200px] md:h-[450px]">
             <Image
               src={ASSETS.acComparison}
               alt="Optimist AC vs Market AC comparison"
